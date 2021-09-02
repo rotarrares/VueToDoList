e9dc4eb1b3008b8cb7dfc71010c7b52d74d4cd79
rares.rotar <rotarrares95@gmail.com>
2021-09-02 15:51:08 +0300

    Feature Delete is complete

--- src/components/TaskListComponent.vue
+++ src/components/TaskListComponent.vue
@@ -1,19 +1,30 @@
 <template>
   <v-layout justify-center>
     <v-list three-line width="500">
-      <v-list-item-group active-class="deep-purple--text text--accent-4">
+      <v-list-item-group>
         <v-list-item
-          @click="checkListItem(task.id)"
+          @change="checkListItem(task.id)"
+          inactive
+          three-line
           :key="task.id"
           v-for="task in $store.getters.getTasks"
         >
           <v-list-item-icon>
-            <v-icon :color="task.completed ? 'green' : 'red'">mdi-flag</v-icon>
+            <v-checkbox
+              :input-value="task.completed"
+              color="green"
+            ></v-checkbox>
           </v-list-item-icon>
           <v-list-item-content align="left">
             <v-list-item-title>{{ task.name }} </v-list-item-title>
             <v-list-item-subtitle>{{ task.description }}</v-list-item-subtitle>
           </v-list-item-content>
+          <v-list-item-action v-show="task.completed">
+            <v-list-item-action-text>Remove</v-list-item-action-text>
+            <v-btn large color="red" icon @click="deleteListItem(task.id)">
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </v-list-item-action>
         </v-list-item>
       </v-list-item-group>
     </v-list>
